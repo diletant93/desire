1357011d313a5daf0b6fd4c7a292445c9e61d07c
Artur <artur.talimonchyk@gmail.com>
2024-10-13 17:45:03 +0200

    advanced righiside menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 export default function setRightsideMenu(){
     const menuButton = document.querySelector('.header__button')
     const rightsideMenu = document.querySelector('.rightside-menu')
+    const rightsideMenuInner = document.querySelector('.rightside-menu__inner')
     const rightsideMenuCloseButton = document.querySelector('.rightside-menu__close')
     
     function openRightSideMenu(){
